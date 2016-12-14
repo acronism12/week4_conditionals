985575790b256eb52ecd8fbc83f4d54c1fce148f
Acronism <alan.barrie@acronism.co.uk>
2016-12-14 19:02:36 +0000

    Updated with corrections after homework review

--- start_point/ruby_conditionals_practice.rb
+++ start_point/ruby_conditionals_practice.rb
@@ -11,28 +11,34 @@ def water_description(temperature)
 end
 
 def calculate_tax(account_balance, account_type)
-  if account_type == "normal"
+  if (account_type == "normal")
     return 0.1 * account_balance
-  else
+  elsif (account_type == "offshore")
     return 0
+  else
+    return account_balance
   end
 end
 
 def hero_movement(hero_race, dice_roll)
   if hero_race == "human"
-    return dice_roll
+    multiplier = 1
+    #return dice_roll
   elsif hero_race == "elf"
-    return dice_roll*2
+    multiplier = 2
+    #return dice_roll*2
   elsif hero_race == "dwarf"
-    return dice_roll/2
+    multiplier = 0.5
+    #return dice_roll/2
   end
+    return dice_roll * multiplier
 end
 
 def should_eat(food_type, season)
-  if food_type == "ice cream" && season == "winter" || food_type == "hot chocolate" && season == "summer"
-    return "false"
+  if (food_type == "ice cream" && season == "winter") || (food_type == "hot chocolate" && season == "summer")
+    return false
   else
-    return "true"
+    return true
   end
 end
 
@@ -41,7 +47,7 @@ def volume_of_cube(side_length)
 end
 
 def volume_of_sphere(radius)
-  return (4*Math::PI*radius**3)/3
+  return (4 * Math::PI * radius**3)/3
 end
 
 def convert_to_celsius(temp_in_f)
